160fd65c8b783cc5dc546339de4070ffd7b1c981
Dewa <dewaasmara589@gmail.com>
2022-11-08 11:16:09 +0700

    Materi Ternary Operator

--- starter/app.js
+++ starter/app.js
@@ -2,21 +2,21 @@
 
 let yourYearOfBirth = Number(prompt("Kapan tahun lahirmu ?"))
 
-// if (2020 - yourYearOfBirth < 17){
-//   alert("Kamu belum cukup umur, karena di bawah 17 tahun")
-// } else {
-//   alert("Kamu sudah cukup umur")
-// }
-
+// Ini adalah sebuah statement
+// Digunakan jika ada lebih dari 2 percabangan
 if (2020 - yourYearOfBirth < 17){
-  alert("Kamu masih kecil")
-} else if (2020 - yourYearOf > 17 &&  2020 - yourYearOf > 40) {
-  alert("Kamu sudah dewasa")
-} else if (2020 - yourYearOf > 40) {
+  alert("Kamu maish kecil")
+} else {
   alert("Kamu sudah tua")
-}else {
-  alert("Tidak Jelas")
 }
+
+// Ternary Operator
+
+// Ini adalah sebuah expression dan dapat di embed
+// Digunakan saat ada 2 percabangan saja
+2020 - yourYearOfBirth < 17 ? alert("Kamu maish kecil") : alert("Kamu sudah tua")
+
+
 
 const projectTitle = document.querySelector("#project-title");
 projectTitle.textContent = "Title Baru dari JS File"
