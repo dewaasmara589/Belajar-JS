a4c284fe944aaf47f74d67449c1291e6ad6cce30
Dewa <dewaasmara589@gmail.com>
2022-11-08 14:36:33 +0700

    Materi Perbedaan function definition dengan function declaration dan Arrow Function

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,7 @@
 // Functions
 
 // Function Definition
+// jika memanggil function ini sebelum dia dibuat maka tidak dapat dipanggil
 const perpangkatan = function(n)  {
   return n*n
 }
@@ -11,16 +12,30 @@ function pangkat(m) {
 }
 
 // Function undefined (side-effect)
+// jika memanggil function ini sebelum dia dibuat maka akan tetap dijalankan
+console.log(sayHello())
+
+
 function sayHello() {
   alert('Hello!')
 }
 
 console.log(perpangkatan(2))
 console.log(pangkat(4))
-console.log(sayHello())
 
 // Menampilkan isi atau value didalam variablenya
 console.log(sayHello)
+
+// ES6 Arrow Function dapat membuat sebuah funtion dalam 1 line saja
+// Cara 1
+// const perpangkatan2 = (k) => {
+//   return k * k
+// }
+
+// Cara 2 Jika hanya 1 variable dan mengembalikan 1 value saja
+const perpangkatan2 = k => k * k
+
+console.log(perpangkatan2(5))
 
 const projectTitle = document.querySelector("#project-title");
 projectTitle.textContent = "Title Baru dari JS File"
