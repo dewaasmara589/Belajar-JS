a8610ffff87275edf95fcbd68a91ba2add8c3b33
Dewa <dewaasmara589@gmail.com>
2022-11-08 14:20:24 +0700

    Materi Function definition and declaration

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,26 @@
-// Loop
+// Functions
 
-for (let i = 0; i <= 100; i = i + 2) {
-  console.log(i)
+// Function Definition
+const perpangkatan = function(n)  {
+  return n*n
 }
 
-for (let i = "*"; i.length <= 5; i = i + "*") {
-  console.log(i)
+// Function Declaration
+function pangkat(m) {
+  return m*m
 }
+
+// Function undefined (side-effect)
+function sayHello() {
+  alert('Hello!')
+}
+
+console.log(perpangkatan(2))
+console.log(pangkat(4))
+console.log(sayHello())
+
+// Menampilkan isi atau value didalam variablenya
+console.log(sayHello)
 
 const projectTitle = document.querySelector("#project-title");
 projectTitle.textContent = "Title Baru dari JS File"
