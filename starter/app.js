6e07fe2360684981f489c2c3644bca2476a8650f
Dewa <dewaasmara589@gmail.com>
2022-11-08 10:56:49 +0700

    Materi Program Control Flow

--- starter/app.js
+++ starter/app.js
@@ -1,9 +1,8 @@
-// Return Value
+// Control Flow
 
-// alert("Ini adakah sebuah alert")
+let yourName = String(prompt("What is your name?"));
 
-// console.log(Math.min(10, 9, 19, 20,5)) // mengembalikan nilai terkecil
-console.log(Math.round(1.4))  // Pembulatan 1.3 menjadi 1, 1.6 menjadi 2
+alert('Hi, so your name is ' + yourName);
 
 const projectTitle = document.querySelector("#project-title");
 projectTitle.textContent = "Title Baru dari JS File"
