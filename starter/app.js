5fd0eb3ff270c6c5b3eed7e806d7f17e64dfa0a2
Dewa <dewaasmara589@gmail.com>
2022-11-08 14:52:11 +0700

    Materi Functional Programming to Change Title

--- starter/app.js
+++ starter/app.js
@@ -1,41 +1,15 @@
-// Functions
+// Functional Programming
 
-// Function Definition
-// jika memanggil function ini sebelum dia dibuat maka tidak dapat dipanggil
-const perpangkatan = function(n)  {
-  return n*n
+const changeProjectHeadingTitle = (title) => {
+  const projectTitle = document.querySelector("#project-title");
+
+  if (typeof title === 'string') {
+    projectTitle.textContent = title;
+  }else {
+    console.error("Your Argument is not a string!")
+  }
+
+  projectTitle.textContent = title
 }
 
-// Function Declaration
-function pangkat(m) {
-  return m*m
-}
-
-// Function undefined (side-effect)
-// jika memanggil function ini sebelum dia dibuat maka akan tetap dijalankan
-console.log(sayHello())
-
-
-function sayHello() {
-  alert('Hello!')
-}
-
-console.log(perpangkatan(2))
-console.log(pangkat(4))
-
-// Menampilkan isi atau value didalam variablenya
-console.log(sayHello)
-
-// ES6 Arrow Function dapat membuat sebuah funtion dalam 1 line saja
-// Cara 1
-// const perpangkatan2 = (k) => {
-//   return k * k
-// }
-
-// Cara 2 Jika hanya 1 variable dan mengembalikan 1 value saja
-const perpangkatan2 = k => k * k
-
-console.log(perpangkatan2(5))
-
-const projectTitle = document.querySelector("#project-title");
-projectTitle.textContent = "Title Baru dari JS File"
+changeProjectHeadingTitle("JavaScipt is Cool")
