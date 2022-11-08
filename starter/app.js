f4217e4e3383ff92aa120e5224d5c91737702a7b
Dewa <dewaasmara589@gmail.com>
2022-11-08 11:07:56 +0700

    Materi Conditional Execution

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,22 @@
-// Control Flow
+// Conditional Execution
 
-let yourName = String(prompt("What is your name?"));
+let yourYearOfBirth = Number(prompt("Kapan tahun lahirmu ?"))
 
-alert('Hi, so your name is ' + yourName);
+// if (2020 - yourYearOfBirth < 17){
+//   alert("Kamu belum cukup umur, karena di bawah 17 tahun")
+// } else {
+//   alert("Kamu sudah cukup umur")
+// }
+
+if (2020 - yourYearOfBirth < 17){
+  alert("Kamu masih kecil")
+} else if (2020 - yourYearOf > 17 &&  2020 - yourYearOf > 40) {
+  alert("Kamu sudah dewasa")
+} else if (2020 - yourYearOf > 40) {
+  alert("Kamu sudah tua")
+}else {
+  alert("Tidak Jelas")
+}
 
 const projectTitle = document.querySelector("#project-title");
 projectTitle.textContent = "Title Baru dari JS File"
