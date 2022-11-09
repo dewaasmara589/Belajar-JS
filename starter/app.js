98faafc6d02f67657d39b2dc98307bc2b867992c
Dewa <dewaasmara589@gmail.com>
2022-11-09 15:45:24 +0700

    Mini Project - Addtodo and DOM CreateElement

--- starter/app.js
+++ starter/app.js
@@ -6,4 +6,32 @@ const filterInput = document.querySelector("#filter-input");
 const todoList = document.querySelector("#todo-list");
 const clearButton = document.querySelector("#clear-button");
 
-console.log(todoForm)
+todoForm.addEventListener("submit", addTodo);
+
+function addTodo(e) {
+  e.preventDefault();
+
+  // Membuat li element
+  const li = document.createElement("li")
+
+  // Menambahkan class pada element li
+  li.className = "list-group-item d-flex justify-content-between align-items-center mb-1"
+
+  // Cara 1 Menambahkan children ke dalam element li
+  li.appendChild(document.createTextNode("Value dari task input"))
+
+  // Membuat delete button
+  const a = document.createElement("a")
+
+  // Memberi properti untuk element
+  a.href = "#"
+  a.className = "badge badge-danger"
+
+  // Cara 2 Menambahkan child ke dalama element
+  a.innerHTML = "Delete"
+
+  // Menyelipkan elemet a ke children li
+  li.appendChild(a)
+
+  console.log(li)
+}
