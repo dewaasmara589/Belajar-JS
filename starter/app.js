22c153d898ed896110b9f0ebfeae74f6e971f979
Dewa <dewaasmara589@gmail.com>
2022-11-09 09:32:53 +0700

    Materi Array Method Push, Pop, Unshift dan Shift

--- starter/app.js
+++ starter/app.js
@@ -1,18 +1,23 @@
 // Array
 
-const array = [2, 4, 6, 18, 19, "sting", "wegodev", true]
+const array = [1, 2, 3, 4, 5]
 
-console.log(array[3])
-console.log(array.length)  // Digunakan untuk melooping array
+// Push method akan menambah sebuah value baru di berlakang array
+array.push(6)
 
-const kaliAngka = [1, 2, 3, 4, 5]
+console.log(array)
 
-for (let i = 0; i < kaliAngka.length; i++) {
-  kaliAngka[i] = kaliAngka[i]*2
-}
+// Pop method menghilangkan sebuah value di belakang array, pop juga mereturn nilai yang dia remove
+array.pop()
+console.log(array.pop())
 
-console.log(kaliAngka)
+console.log(array)
 
-kaliAngka[0] = "satu";
+// Unshift dan shift method menambahkan dan menghapus sebuah value dari depan
+array.unshift(0)
 
-console.log(kaliAngka)
+console.log(array)
+
+array.shift()
+
+console.log(array)
