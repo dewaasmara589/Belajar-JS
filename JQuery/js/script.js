e1c0a5f9c4c17ad0cb8d4e681a995f0cca157571
Dewa <dewaasmara589@gmail.com>
2022-11-22 15:05:32 +0700

    Materi JQeury - Javascript, Velocity & Animation (Button Start)

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -1,5 +1,20 @@
 function animationIntro() {
-    $("#text span").velocity("transition.slideLeftIn", { stagger: 250 });
+    $("#text span").velocity("transition.slideLeftIn",{ 
+                                stagger: 250,
+                                complete: function(){
+                                    animationButtonStart();
+                                }
+                            });
+}
+
+function animationButtonStart() {
+    $("#start").velocity("transition.bounceUpIn")
+                .mouseenter(function(){
+                    $(this).velocity({width:100});
+                })
+                .mouseleave(function(){
+                    $(this).velocity({width:125});
+                });
 }
 
 $(document).ready(function() {
