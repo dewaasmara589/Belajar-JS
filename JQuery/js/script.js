d73ff75b90afb63cf32cd2663c9cf5b513c8fd50
Dewa <dewaasmara589@gmail.com>
2022-11-23 09:18:50 +0700

    Materi JQeury - Menu

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -19,12 +19,20 @@ function animationButtonStart() {
 
 function animasiIntroOut() {
     $("#start").velocity("transition.whirlOut",{ 
-                            stagger: 250,
+                            stagger: 150,
                             complete: function(){
                                 $("#text").velocity({"font-size":"20px", "top":"95%"},
-                                                    {duration: 1000})
+                                                    {duration: 1000,
+                                                    complete: function(){
+                                                        callMenu()
+                                                    }
+                                                    })
                             }
                         });
+}
+
+function callMenu(){
+    $("#menu ul li").velocity("transition.slideLeftIn",{stagger: 150});
 }
 
 $(document).ready(function() {
