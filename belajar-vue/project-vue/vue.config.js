11c290735b82153eb754d46844cf0f7764fd9e52
Dewa <dewaasmara589@gmail.com>
2022-11-24 16:06:48 +0700

    Materi Vue - Deploy Project Vue

--- belajar-vue/project-vue/vue.config.js
+++ belajar-vue/project-vue/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// Agar file index.html dapat dijalankan
+module.exports = {
+  publicPath: './'
+}
